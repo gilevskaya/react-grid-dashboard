4f1df65b93e9f041e5cbd48cfcc088474a3c3909
Alena Gilevskaya <alena@gilevskaya.com>
2020-06-23 00:30:42 -0700

    Updates

---
+++ precache-manifest.0e0c8e7828664ab82573121e8c83cfd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51d4e5d7765146d8f1ae44b4363609f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3382d572dbeeecd2bcd",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "dfadbe0530c8e10ca3d7",
+    "url": "./static/js/2.5c5dbc87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5c5dbc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3382d572dbeeecd2bcd",
+    "url": "./static/js/main.b9993ffd.chunk.js"
+  },
+  {
+    "revision": "56cd24cab6a194bf29cc",
+    "url": "./static/js/runtime-main.1ae57a79.js"
+  }
+]);
