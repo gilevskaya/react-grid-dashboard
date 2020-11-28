1040042bb77929304d5dc5ab26a1b192cfbc7188
Alena Gilevskaya <alena@gilevskaya.com>
2020-11-28 12:35:03 -0800

    ts example - console.log

---
+++ example-ts/src/App.tsx
@@ -0,0 +1,7 @@
+import * as dashboard from "react-grid-dashboard";
+
+console.log({ dashboard });
+
+export const App = () => {
+  return <div>App</div>;
+};
---
+++ example-ts/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+
+import { App } from "./App";
+console.log("Hello!!!");
+
+ReactDOM.render(<App />, document.getElementById("root"));
