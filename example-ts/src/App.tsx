dcbc3567e3e0a596828622d2b0971cdb46598729
Alena Gilevskaya <alena@gilevskaya.com>
2020-11-28 17:37:52 -0800

    cleanup

--- example-ts/src/App.tsx
+++ example-ts/src/App.tsx
@@ -1,8 +1,4 @@
-import * as DashboardRe from "react-grid-dashboard";
-
-console.log({ DashboardRe });
-// Dashboard.Item = DashboardRe.Item.make;
-// Dashboard.Test = DashboardRe.Test.make;
+import Grid from "react-grid-dashboard";
 
 const layout = {
   // w=1 h=1 – default and can be ommited
@@ -16,55 +12,55 @@ const layout = {
 export const App = () => {
   return (
     <div style={{ height: "100vh" }}>
-      123
-      {/* <Test /> */}
-      {/* <Dashboard columns={12} rows={12} gap="10px">
-        <Dashboard.Item {...layout.menu}>
-          <div
-            style={{
-              padding: 10,
-              background: "Turquoise",
-              height: "100%",
-            }}
-          >
-            Fixed height menu
-          </div>
-        </Dashboard.Item>
-        <Dashboard.Item {...layout.a}>
-          <div
-            style={{
-              padding: 10,
-              background: "LightSkyBlue",
-              height: "100%",
-            }}
-          >
-            Component A
-          </div>
-        </Dashboard.Item>
-        <Dashboard.Item {...layout.b}>
-          <div
-            style={{
-              padding: 10,
-              background: "LightGreen",
-              height: "100%",
-              overflowY: "auto", // adds scrollable
-            }}
-          >
-            {"Component B, scrollable; ".repeat(100)}
-          </div>
-        </Dashboard.Item>
-        <Dashboard.Item {...layout.c}>
-          <div
-            style={{
-              padding: 10,
-              background: "LightGray",
-              height: "100%",
-            }}
-          >
-            Component C
-          </div>
-        </Dashboard.Item>
-      </Dashboard> */}
+      <Grid.Dashboard columns={12} rows={12} gap="10px">
+        <>
+          <Grid.Item {...layout.menu}>
+            <div
+              style={{
+                padding: 10,
+                background: "Turquoise",
+                height: "100%",
+              }}
+            >
+              Fixed height menu
+            </div>
+          </Grid.Item>
+          <Grid.Item {...layout.a}>
+            <div
+              style={{
+                padding: 10,
+                background: "LightSkyBlue",
+                height: "100%",
+              }}
+            >
+              Component A
+            </div>
+          </Grid.Item>
+          <Grid.Item {...layout.b}>
+            <div
+              style={{
+                padding: 10,
+                background: "LightGreen",
+                height: "100%",
+                overflowY: "auto", // adds scrollable
+              }}
+            >
+              {"Component B, scrollable; ".repeat(100)}
+            </div>
+          </Grid.Item>
+          <Grid.Item {...layout.c}>
+            <div
+              style={{
+                padding: 10,
+                background: "LightGray",
+                height: "100%",
+              }}
+            >
+              Component C
+            </div>
+          </Grid.Item>
+        </>
+      </Grid.Dashboard>
     </div>
   );
 };
--- src/test/Test.res
+++
@@ -1,6 +0,0 @@
-Js.log("Test!")
-
-@genType @react.component
-export make = () => {
-  <div> {"TEST"->React.string} </div>
-}
